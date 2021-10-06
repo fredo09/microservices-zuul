7e0e4f3bb85ffac86c5ede1e190adf5d8bb01c5a
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2021-10-05 19:08:27 -0500

    Initial commit

---
+++ src/test/java/com/fredy/mircroservices/app/zuul/MicroservicesZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredy.mircroservices.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
